617142bf503f5a47e8134621802c05416fbc188d
japson92 <sebastianlefek92@gmail.com>
2022-08-14 22:48:09 +0200

    code refactoring moving some methods out of mainwwindow class

--- database/App.py
+++ database/App.py
@@ -1,9 +1,22 @@
 import MainWindow
 
 startApp = MainWindow.MainWindow()
-text = ["Cat Nr", "Company Name", "Tool Type"]
-for labels in range(3):
-    startApp.create_labels(labels, 3, text[labels])
+text = ["Cat Nr", "Company Name", "Tool Type", "Amount"]
+text2 = ["New Company", "", "New Tool Type"]
+button_column = [2, 1, 3]
+button_text = ["Add Tool", "Add Company", "Add Tool Type"]
+button_row = [2, 6, 6]
+button_methods = [startApp.add_new_tool, startApp.add_new_company, startApp.add_new_tool_type]
+for labels in range(4):
+    startApp.create_labels(labels, 0, text[labels])
 
+for labels in range(0,4,2):
+    startApp.create_labels(labels, 3, text2[labels])
+for button in range(3):
+    startApp.create_buttons(button_text[button], button_methods[button], button_column[button], button_row[button])
+
+startApp.load_json_file_dict("data_dict.json")
+startApp.load_json_company_file("company_list.json")
+startApp.load_json_tools_file("tool_type_list.json")
 
 startApp.win.mainloop()
--- database/MainWindow.py
+++ database/MainWindow.py
@@ -4,28 +4,27 @@ from tkinter import messagebox as mBox
 import json
 
 
+def save_json_file(fileName, dataList):
+    with open(fileName, "w", encoding="UTF-8") as file:
+        json.dump(dataList, file, ensure_ascii=False, indent=4, sort_keys=True)
+
+
 class MainWindow:
     def __init__(self):
         self.win = tk.Tk()
         self.win.title("Tools Database")
         self.create_window()
         self.create_text_boxes()
-
-        self.create_buttons()
         self.create_combo_box()
-        self.load_json_file_dict("data_dict.json")
-        self.load_json_company_file("company_list.json")
-        self.load_json_tools_file("tool_type_list.json")
-
+        self.data_list = []
+        self.data_dict = {}
+        self.company_list = []
+        self.tool_type_list = []
 
     def _quit(self):
         self.win.quit()
         self.win.destroy()
         exit()
-
-    def save_json_file(self, fileName, dataList):
-        with open(fileName, "w", encoding="UTF-8") as file:
-            json.dump(dataList, file, ensure_ascii=False, indent=4, sort_keys=True)
 
     def load_json_file_dict(self, fileName):
         with open(fileName, encoding="UTF-8") as file:
@@ -37,31 +36,28 @@ class MainWindow:
     def load_json_company_file(self, fileName):
         with open(fileName, encoding="UTF-8") as file:
             self.company_list = json.load(file)
-            print("loaded")
-            print(self.company_list)
             self.combo_data2["values"] = self.company_list
 
     def load_json_tools_file(self, fileName):
         with open(fileName, encoding="UTF-8") as file:
             self.tool_type_list = json.load(file)
-            print("loaded")
-            print(self.tool_type_list)
             self.combo_data3["values"] = self.tool_type_list
 
     def add_new_tool(self):
-        cat_nr = self.etb0.get()
-        company_name = self.combo_data2.get()
-        tool_type = self.combo_data3.get()
-        amount = self.etb3.get()
-        if cat_nr or company_name or tool_type or amount:
-            self.data_list.append(cat_nr)
-            self.data_dict.update({cat_nr: {"company_name": company_name, "tool_type": tool_type, "amount": amount}})
+        input_list = []
+        elements_list = [self.etb0, self.combo_data2, self.combo_data3, self.etb3]
+        for i in range(len(elements_list)):
+            input_list.append(elements_list[i].get())
+
+        if input_list[0] and input_list[1] and input_list[2] and input_list[3]:
+            self.data_list.append(input_list[0])
+            self.data_dict.update({input_list[0]: {"company_name": input_list[1], "tool_type": input_list[2],
+                                                   "amount": input_list[3]}})
             self.combo_data["values"] = self.data_list
-            self.etb0.delete(first=0, last=len(cat_nr))
-            self.combo_data2.delete(first=0, last=len(company_name))
-            self.combo_data3.delete(first=0, last=len(tool_type))
-            self.etb3.delete(first=0, last=len(amount))
-            self.save_json_file("data_dict.json", self.data_dict)
+            for element in range(4):
+                elements_list[element].delete(first=0, last=(len(input_list[element])))
+
+            save_json_file("data_dict.json", self.data_dict)
 
     def add_new_company(self):
         company = self.etb1.get()
@@ -70,16 +66,15 @@ class MainWindow:
             print("dodano")
             self.combo_data2["values"] = self.company_list
             self.etb1.delete(first=0, last=len(company))
-            self.save_json_file("company_list.json", self.company_list)
+            save_json_file("company_list.json", self.company_list)
 
     def add_new_tool_type(self):
         tool_type = self.etb2.get()
         if tool_type:
             self.tool_type_list.append(tool_type)
-            print("dodano")
             self.combo_data3["values"] = self.tool_type_list
             self.etb2.delete(first=0, last=len(tool_type))
-            self.save_json_file("tool_type_list.json", self.tool_type_list)
+            save_json_file("tool_type_list.json", self.tool_type_list)
 
     def change_label(self):
         picked_tool = self.combo_data.get()
@@ -91,17 +86,18 @@ class MainWindow:
     def release_tool(self):
         picked_tool = self.combo_data.get()
         dictionary = self.data_dict[picked_tool]
-
         new_amount = int(dictionary["amount"]) - 1
+
         if new_amount < 5:
             mBox.showwarning("Warning", "Low amount of tool!")
+
         dictionary["amount"] = str(new_amount)
 
         self.labelka.configure(text="Catalog number: " + picked_tool + ", company name: " +
                                     dictionary["company_name"] + ", tool type: " +
                                     dictionary["tool_type"] + ", amount: " + dictionary["amount"])
 
-        self.save_json_file("data_dict.json", self.data_dict)
+        save_json_file("data_dict.json", self.data_dict)
         mBox.showinfo("Success!", "Tool release done")
 
     def create_window(self):
@@ -121,10 +117,10 @@ class MainWindow:
         self.monty = ttk.LabelFrame(tab1, text=' Add new tools ')
         self.monty.grid(column=0, row=0, padx=8, pady=4)
 
-        self.monty2 = ttk.LabelFrame(tab1, text=' Add new Company ')
+        self.monty2 = ttk.LabelFrame(tab2, text=' Add new Company ')
         self.monty2.grid(column=0, row=1, padx=8, pady=4, sticky="W")
 
-        self.monty3 = ttk.LabelFrame(tab1, text=' Add new Tool type ')
+        self.monty3 = ttk.LabelFrame(tab3, text=' Add new Tool type ')
         self.monty3.grid(column=0, row=2, padx=8, pady=4, sticky="W")
 
         # Creating a Menu Bar
@@ -140,19 +136,18 @@ class MainWindow:
 
     def create_text_boxes(self):
         self.etb0 = ttk.Entry(self.monty, width=12)
-        self.etb0.grid(column=0, row=4)
+        self.etb0.grid(column=0, row=1)
 
-        self.etb1 = ttk.Entry(self.monty2, width=12)
-        self.etb1.grid(column=0, row=1)
+        self.etb1 = ttk.Entry(self.monty, width=12)
+        self.etb1.grid(column=0, row=6)
 
-        self.etb2 = ttk.Entry(self.monty3, width=12)
-        self.etb2.grid(column=0, row=1)
+        self.etb2 = ttk.Entry(self.monty, width=12)
+        self.etb2.grid(column=2, row=6)
 
         self.etb3_name = tk.IntVar()
         self.etb3 = ttk.Entry(self.monty, width=12, textvariable=self.etb3_name)
         self.etb3.delete(first=0, last=1)
-        self.etb3.grid(column=3, row=4)
-
+        self.etb3.grid(column=3, row=1)
 
         self.etb0.focus()
 
@@ -160,52 +155,25 @@ class MainWindow:
         label = ttk.Label(self.monty, text=text)
         label.grid(column=column, row=row)
 
-        # label1 = ttk.Label(self.monty, text="Cat Nr")
-        # label1.grid(column=0, row=3)
-        # label2 = ttk.Label(self.monty, text="Company Name")
-        # label2.grid(column=1, row=3)
-        # label3 = ttk.Label(self.monty, text="Tool Type")
-        # label3.grid(column=2, row=3)
-        label4 = ttk.Label(self.monty, text="Amount")
-        label4.grid(column=3, row=3)
-
-        self.labelka = ttk.Label(self.monty, text="")
+        self.labelka = ttk.Label(self.monty2, text="")
         self.labelka.grid(column=0, row=6, columnspan=4)
 
     def create_combo_box(self):
-        self.data_list = []
-        self.data_dict = {}
-        self.company_list = []
-        self.tool_type_list = ["drill", "wkret"]
-
-        self.combo_data = ttk.Combobox(self.monty, width=12)
+        self.combo_data = ttk.Combobox(self.monty2, width=12)
         self.combo_data.grid(column=1, row=1)
 
         self.combo_data2 = ttk.Combobox(self.monty, width=12)
-        self.combo_data2.grid(column=1, row=4)
-
+        self.combo_data2.grid(column=1, row=1)
 
         self.combo_data3 = ttk.Combobox(self.monty, width=12)
-        self.combo_data3.grid(column=2, row=4)
-        self.combo_data3["values"] = self.tool_type_list
+        self.combo_data3.grid(column=2, row=1)
 
-    def create_buttons(self):
-        self.bt1 = ttk.Button(
-            self.monty, text="Add Tool!", command=self.add_new_tool)
-        self.bt1.grid(column=2, row=1)
+    def create_buttons(self, buttonText, buttonCommand, column, row):
+        bt1 = ttk.Button(self.monty, text=buttonText, command=buttonCommand)
+        bt1.grid(column=column, row=row)
 
-        self.bt2 = ttk.Button(
-            self.monty, text="Change label!", command=self.change_label)
-        self.bt2.grid(column=2, row=5)
+        bt2 = ttk.Button(self.monty2, text="Change label!", command=self.change_label)
+        bt2.grid(column=2, row=5)
 
-        self.bt3 = ttk.Button(
-            self.monty, text="Release!", command=self.release_tool)
-        self.bt3.grid(column=3, row=5)
-
-        self.bt4 = ttk.Button(
-            self.monty2, text="Add Company", command=self.add_new_company)
-        self.bt4.grid(column=1, row=1)
-
-        self.bt5 = ttk.Button(
-            self.monty3, text="Add Tool Type", command=self.add_new_tool_type)
-        self.bt5.grid(column=2, row=1)
+        bt3 = ttk.Button(self.monty2, text="Release!", command=self.release_tool)
+        bt3.grid(column=3, row=5)
